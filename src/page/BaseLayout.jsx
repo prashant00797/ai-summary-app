d7faba6597827d5c6db788c2a4988712dcef9bea
prashant00797 <prashantnath6307@gmail.com>
2026-03-18 11:58:26 +0530

    cta cum footer added

--- src/page/BaseLayout.jsx
+++ src/page/BaseLayout.jsx
@@ -1,15 +1,19 @@
+import Footer from "../ui/Footer";
 import HeaderUI from "../ui/HeaderUI";
 import { GeminiLogo } from "../utility/icons";
 import SearchLayout from "./SearchLayout";
 
 const BaseLayout = () => {
   return (
-    <div className="app-container">
-      <div className="absolute top-0 right-1">
-        <GeminiLogo className="w-15 md:w-30  h-auto" />
+    <div className="min-h-screen flex flex-col">
+      <div className="app-container">
+        <div className="absolute top-0 right-1">
+          <GeminiLogo className="w-15 md:w-30  h-auto" />
+        </div>
+        <HeaderUI />
+        <SearchLayout />
       </div>
-      <HeaderUI />
-      <SearchLayout />
+      <Footer />
     </div>
   );
 };
---
+++ src/ui/Footer.jsx
@@ -0,0 +1,29 @@
+const Footer = () => {
+  const contactData = {
+    headerLabel: "© 2026 Insight AI ·",
+    links: [
+      {
+        name: "Feedback ·",
+        link: "mailto:prashantnath6307@gmail.com",
+      },
+      {
+        name: "Github",
+        link: "https://github.com/prashant00797",
+      },
+    ],
+  };
+  return (
+    <div className="text-center text-sm text-gray-500 py-4 tracking-wide">
+      {contactData.headerLabel}
+      {contactData.links.map((link) => {
+        return (
+          <a href={link.link} className="hover:text-primary-hover">
+            {link.name}
+          </a>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Footer;
